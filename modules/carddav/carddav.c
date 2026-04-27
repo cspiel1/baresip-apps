371024fe924c49c4653cea39779f418f3a805766
joe.a.burmeister@gmail.com <joe.a.burmeister@gmail.com>
2026-04-27 23:15:00 +0100

    Add uploading contacts to CardDAV if only in baresip. Try as phone or SIP/IM.

--- modules/carddav/carddav.c
+++ modules/carddav/carddav.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <curl/curl.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
@@ -22,7 +23,7 @@
  *
  * App module add carddav commands using libcurl
  *
- * This module adds command to refresh contacts from a CardDAV.
+ * This module adds command to refresh contacts from a "(CardDAV)".
  *
  * Example config:
  \verbatim
@@ -46,8 +47,12 @@ struct carddav_context
 	char * buf_a;
 	char * buf_b;
 	char * gateway;
+	const char * user;
+	const char * url;
 	unsigned count;
 };
+
+#define CARDDAV "(CardDAV)"
 
 
 static bool get_vcard_attr(const char * cardstart,
@@ -89,61 +94,126 @@ static bool get_vcard_attr(const char * cardstart,
 }
 
 
+static struct contact  * in_contacts(struct contacts * contacts,
+                                     const char * uri)
+{
+	struct pl pl;
+	struct sip_addr addr;
+
+	pl_set_str(&pl, uri);
+
+	int err = sip_addr_decode(&addr, &pl);
+	if (err)
+		return NULL;
+
+	char safe[128] = {0};
+
+	str_ncpy(safe, addr.auri.p, addr.auri.l + 1);
+
+	return contact_find(contacts, safe);
+}
+
+
+static char * last_column(char * s)
+{
+	unsigned len = strlen(s);
+	while (len) {
+		char c = s[--len];
+		if (c == ';' || c == ':')
+			return s + len;
+	}
+
+	return NULL;
+}
+
+
 static int process_card(const char * cardstart,
                          uintptr_t cardend,
                          struct carddav_context * context)
 {
 	char name[512] = {0};
 	char tel[512] = {0};
+	char addr[1024] = {0};
+
 	if (!get_vcard_attr(cardstart, cardend, "\nFN", name))
 		return EINVAL;
-	if (!get_vcard_attr(cardstart, cardend, "\nTEL", tel))
+
+	if (get_vcard_attr(cardstart, cardend, "\nIMPP", tel)) {
+		char * pos = last_column(tel);
+		if (!pos)
+			return EINVAL;
+		pos -= 3;
+		if (pos < tel) {
+			debug("carddav: Broken IMPP for %s\n", name);
+			return EINVAL;
+		}
+
+		if (!strncmp(pos, "SIP:", 4) || !strncmp(pos, "sip:", 4))
+		{
+			pos[0]=tolower(pos[0]);
+			pos[1]=tolower(pos[1]);
+			pos[2]=tolower(pos[2]);
+			debug("carddav: SIP IMPP <%s>\n", pos);
+			re_snprintf(addr,
+				   sizeof(addr),
+				   "\"%s "CARDDAV"\" <%s>",
+				   name, pos);
+		}
+		else debug("carddav: Non-SIP IMPP %s\n", tel);
+	}
+
+	if (!addr[0] && get_vcard_attr(cardstart, cardend, "\nTEL", tel)) {
+		const char * pos = last_column(tel);
+		if (!pos)
+			return EINVAL;
+		unsigned len = re_snprintf(addr,
+		                           sizeof(addr),
+		                           "\"%s "CARDDAV"\" <sip:",
+		                           name);
+		unsigned hascode=0;
+
+		while (*pos) {
+			char c = *pos++;
+
+			if ( c == '+') {
+				addr[len++]='0';
+				++hascode;
+			}
+			else if (isdigit(c)) {
+				if (hascode==1) {
+					addr[len++]='0';
+					hascode=0;
+				}
+				else if (hascode>2) {
+					len-=(hascode-2);
+					hascode=0;
+				}
+				addr[len++]=c;
+			}
+		}
+
+		re_snprintf(addr+len, sizeof(addr)-len,
+			    "@%s>", context->gateway);
+	}
+
+	if (!addr[0])
 		return EINVAL;
 
-	unsigned long pos = strlen(tel);
-	while (pos) {
-		if (tel[pos] == ':' || tel[pos] == ':') {
-			char addr[1024] = {0};
-			struct pl pl;
-			unsigned len = re_snprintf(addr,
-			                           sizeof(addr),
-			                           "\"%s\" <sip:",
-			                           name);
-			++pos;
-			unsigned hascode=0;
-			while (tel[pos]) {
-				char c = tel[pos++];
-				if ( c == '+') {
-					addr[len++]='0';
-					++hascode;
-				}
-				else if (isdigit(c)) {
-					if (hascode==1) {
-						addr[len++]='0';
-						hascode=0;
-					}
-					else if (hascode>2) {
-						len-=(hascode-2);
-						hascode=0;
-					}
-					addr[len++]=c;
-				}
-			}
-			re_snprintf(addr+len, sizeof(addr)-len,
-			            "@%s>", context->gateway);
-
-			pl_set_str(&pl, addr);
-			info("carddav: Adding %s\n", addr);
-			int e = contact_add(context->contacts, NULL, &pl);
-			if (!e)
-				context->count++;
-			else
-				warning("carddav: Failed to add contact.\n");
-			return e;
-		}
-		--pos;
+	if (in_contacts(context->contacts, addr)) {
+		info("carddav: Duplicate SIP %s\n", addr);
+		return 0;
 	}
-	return EINVAL;
+
+	struct pl pl;
+	pl_set_str(&pl, addr);
+
+	info("carddav: Adding %s\n", addr);
+	int e = contact_add(context->contacts, NULL, &pl);
+	if (!e)
+		context->count++;
+	else
+		warning("carddav: Failed to add contact.\n");
+	return e;
 }
 
 
@@ -229,6 +299,225 @@ static size_t writefunc(const void *ptr,
 }
 
 
+static void move_contacts(struct list * contacts_a,
+                          struct list * contacts_b,
+                          struct contacts * contacts)
+{
+	info("carddav: wipe existing contacts.\n");
+	struct le * cur = list_head(contacts_a);
+
+	while (cur) {
+		struct le * next = cur->next;
+		mem_ref(cur->data);
+		contact_remove(contacts, cur->data);
+		if (contacts_b)
+			list_prepend(contacts_b, cur, cur->data);
+		cur = next;
+	}
+}
+
+
+static void upload(struct carddav_context * context, const char * name)
+{
+	CURL *curl = curl_easy_init();
+	if (curl) {
+		unsigned len = re_snprintf(context->buf_b,
+		                           context->buf_len,
+		                           "%s/",
+		                           context->url);
+		unsigned namelen = strlen(name);
+
+		if (len + namelen + 5 > context->buf_len) {
+			warning("carddav: buffer to small for upload!\n");
+			return;
+		}
+
+		for (unsigned n = 0; n < namelen; n++) {
+			char c = name[n];
+			if (isalnum(c))
+				context->buf_b[len++] = tolower(c);
+			else
+				context->buf_b[len++] = '_';
+		}
+
+		str_ncpy(&context->buf_b[len], ".vcf", context->buf_len - len);
+		debug("carddav: uploading %s\n", context->buf_b);
+
+		curl_easy_setopt(curl, CURLOPT_URL, context->buf_b);
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+		curl_easy_setopt(curl, CURLOPT_USERPWD, context->user);
+
+		struct curl_slist *hs;
+		hs = curl_slist_append(NULL, "Content-Type: text/vcf");
+
+		curl_easy_setopt(curl, CURLOPT_HTTPHEADER, hs);
+		curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
+
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, context->buf_a);
+
+		CURLcode result = curl_easy_perform(curl);
+		if (result == CURLE_OK)
+			debug("carddav: Uploaded %s\n", name);
+		else
+			warning("carddav: Upload %s failed: %s\n",
+			        name, curl_easy_strerror(result));
+	}
+}
+
+
+static void upload_phone_contact(struct carddav_context * context,
+                                 const char * name,
+                                 const char * phonenumber)
+{
+	re_snprintf(context->buf_a,
+	            context->buf_len,
+	            "BEGIN:VCARD\n"
+	            "VERSION:3.0\n"
+	            "FN:%s\n"
+	            "TEL;TYPE=cell:%s\n"
+	            "END:VCARD\n",
+	            name, phonenumber);
+
+	upload(context, name);
+}
+
+
+static void upload_sip_contact(struct carddav_context * context,
+                               const char * name,
+                               const char * uri)
+{
+/*
+  Good note:
+  https://github.com/basepeak/roundcube-carddav/blob/main/doc/devdoc/IMPP.md
+  IMPP;TYPE=SIP:johndoe@aol.com
+*/
+
+	re_snprintf(context->buf_a,
+	            context->buf_len,
+	            "BEGIN:VCARD\n"
+	            "VERSION:3.0\n"
+	            "FN:%s\n"
+	            "IMPP;TYPE=SIP:%s\n"
+	            "END:VCARD\n",
+	            name, uri + 4);
+
+	upload(context, name);
+}
+
+
+static void extract_name(char name[64], const char * con_str)
+{
+	const char * pos = con_str + 1;
+	const char * end = strchr(pos, '"');
+	unsigned len = PTRDIFF(end, pos) + 1;
+	str_ncpy(name, pos, len);
+}
+
+
+static void upload_unique(struct carddav_context * context,
+                          struct list * contacts_org,
+                          bool just_restore)
+{
+	struct contacts *contacts = context->contacts;
+
+	for (struct le * cur = list_head(contacts_org);
+	    cur;
+	    cur = cur->next) {
+		struct contact * con = (struct contact*)cur->data;
+		const char * con_str = contact_str(con);
+		struct pl pl;
+
+		if (!just_restore) {
+			const char * uri = contact_uri(con);
+
+			if (strstr(con_str, CARDDAV))
+				continue;
+
+			if (in_contacts(context->contacts, uri)) {
+				debug("carddav: Found \"%s\", not adding.\n",
+				      uri);
+				continue;
+			}
+
+			struct contact  * dup = contact_find(contacts, uri);
+			if (dup)
+				continue;
+
+			const char * end = strchr(uri, '@');
+			if (!end) {
+				warning("carddav: Contact URI %s, no @\n",
+				        uri);
+				continue;
+			}
+
+			const char * pos = uri;
+			if (strncmp(pos, "sip:", 4)) {
+				warning("carddav: Contact URI %s, no sip:\n",
+				        uri);
+				continue;
+			}
+
+			pos+=4;
+			while (end && pos < end) {
+				if (!isdigit(*pos)) {
+					debug("carddav: Non-number URI %s\n",
+					      uri);
+					break;
+				}
+				++pos;
+			}
+
+			char name[64] = {0};
+			extract_name(name, con_str);
+
+			if (pos == end) {
+				unsigned len = PTRDIFF(end, uri) - 3;
+				char pn[16] = {0};
+
+				str_ncpy(pn, uri+4, len);
+
+				re_snprintf(context->buf_a,
+				            context->buf_len,
+				            "sip:%s@%s",
+				            pn, context->gateway);
+
+				dup = contact_find(contacts, context->buf_a);
+				if (dup) {
+					debug("carddav: Phone number %s"
+					      " already on gateway.\n", pn);
+					continue;
+				}
+
+				debug("carddav: Push Name \"%s\" "
+				      "Phone number %s\n",
+				      name,  pn);
+				upload_phone_contact(context, name, pn);
+			}
+			else {
+				upload_sip_contact(context, name, uri);
+			}
+
+			re_snprintf(context->buf_a,
+				    context->buf_len,
+				    "\"%s "CARDDAV"\" <%s>",
+				    name, uri);
+
+			info("carddav: Adding as back %s\n", context->buf_a);
+			pl_set_str(&pl, context->buf_a);
+		}
+		else {
+			info("carddav: Adding back %s\n", con_str);
+			pl_set_str(&pl, con_str);
+		}
+
+		int e = contact_add(contacts, NULL, &pl);
+		if (e)
+			warning("carddav: Failed to add back %.*s : %s\n",
+			        pl.l, pl.p, strerror(e));
+	}
+}
+
+
 static int carddav_sync(void)
 {
 	struct carddav_context context = {0};
@@ -242,7 +531,7 @@ static int carddav_sync(void)
 	                 user, sizeof(user)) ||
 	    conf_get_str(conf_cur(), "carddav_url",
 	                 url, sizeof(url))) {
-		warning("carddav: Missing config.\n");
+		warning("carddav: Miss⅞ing config.\n");
 		return EINVAL;
 	}
 
@@ -257,6 +546,8 @@ static int carddav_sync(void)
 	info("carddav: using buffer of: %u\n", context.buf_len);
 	context.contacts = baresip_contacts();
 	context.gateway = gateway;
+	context.url = url;
+	context.user = user;
 
 	CURLcode result = curl_global_init(CURL_GLOBAL_ALL);
 	if (result != CURLE_OK)
@@ -295,21 +586,35 @@ static int carddav_sync(void)
 		                 "</prop>"
 		                 "</propfind>");
 
+
+		struct list * contacts_list = contact_list(context.contacts);
+		struct list contacts_list_org;
+		list_init(&contacts_list_org);
+		move_contacts(contacts_list,
+		              &contacts_list_org,
+		              context.contacts);
+
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
 		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &context);
-
-		info("carddav: wipe existing contacts.\n");
-		list_flush(contact_list(context.contacts));
-
 		result = curl_easy_perform(curl);
-		if (result != CURLE_OK)
-			warning("curl_easy_perform() failed: %s\n",
-			        curl_easy_strerror(result));
-		info("carddav: Added %u contacts.\n", context.count);
-		debug("carddav: curl complete.\n");
-
 		curl_easy_cleanup(curl);
+		if (result == CURLE_OK) {
+			info("carddav: Added %u contacts.\n", context.count);
+			debug("carddav: curl complete.\n");
+			upload_unique(&context, &contacts_list_org,
+			              false);
+			list_flush(&contacts_list_org);
+		}
+		else {
+			warning("carddav: Download failed: %s\n",
+			        curl_easy_strerror(result));
+			move_contacts(contacts_list, NULL, context.contacts);
+			upload_unique(&context, &contacts_list_org,
+			              true);
+			list_flush(&contacts_list_org);
+		}
 	}
+
 	curl_global_cleanup();
 
 	mem_deref(context.buf_a);
